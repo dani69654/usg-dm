921fddb054c5f0fec771e5d3d8880991d6a11959
dani <martinelli.daniele7@gmail.com>
2022-12-28 10:25:48 +0100

    f

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import theme from './theme';
+import { BrowserRouter } from 'react-router-dom'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>
 );
